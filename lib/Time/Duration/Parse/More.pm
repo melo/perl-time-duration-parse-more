f9d93f771efb0c909ba6a6d5a0752720f421999a
Pedro Melo <melo@simplicidade.org>
2013-01-05 20:51:46 +0000

    Build results of 416a26b (on master)

--- lib/Time/Duration/Parse/More.pm
+++ lib/Time/Duration/Parse/More.pm
@@ -6,6 +6,66 @@ our $AUTHORITY = 'cpan:MELO'; # AUTHORITY
 
 use strict;
 use warnings;
+use Exporter;
+use Carp;
+
+our @ISA    = qw( Exporter );
+our @EXPORT = qw( parse_duration );
+
+# From Time::Duration::Parse
+my %units = (
+  map(($_, 1),                  qw(s second seconds sec secs)),
+  map(($_, 60),                 qw(m minute minutes min mins)),
+  map(($_, 60 * 60),            qw(h hr hour hours)),
+  map(($_, 60 * 60 * 24),       qw(d day days)),
+  map(($_, 60 * 60 * 24 * 7),   qw(w week weeks)),
+  map(($_, 60 * 60 * 24 * 30),  qw(M month months)),
+  map(($_, 60 * 60 * 24 * 365), qw(y year years))
+);
+
+my %cache;
+
+sub parse_duration {
+  my ($expression) = @_;
+  return unless defined $expression;
+
+  return $cache{$expression} if exists $cache{$expression};
+  return $cache{$expression} = parse_duration_nc($expression);
+}
+
+sub parse_duration_nc {
+  my ($expression) = @_;
+  return unless defined $expression;
+
+  my $e = $expression;
+  $e =~ s/\band\b/ /gi;
+  $e =~ s/[\s\t]+/ /g;
+  $e =~ s/^\s+|\s+$//g;
+
+  $e =~ s/^\s*([-+]?\d+(?:[.,]\d+)?)\s*$/$1s/;
+  $e =~ s/^\s*([-+]?[.,]\d+)\s*$/$1s/;
+  $e =~ s/\b(\d+):(\d+):(\d+)\b/$1h $2m $3s/g;
+  $e =~ s/\b(\d+):(\d+)\b/$1h $2m/g;
+
+  my $duration = 0;
+  my $signal   = 1;
+  while ($e) {
+    if    ($e =~ s/^plus\b(\s*,?)*//)  { $signal = 1 }
+    elsif ($e =~ s/^minus\b(\s*,?)*//) { $signal = -1 }
+    elsif ($e =~ s/^(([-+]?\d+(?:[,.]\d*)?)\s*(\w+))(\s*,?)*// or $e =~ s/^(([-+]?[,.]\d+)\s*(\w+))(\s*,?)*//) {
+      my ($m, $n, $u) = ($1, $2, $3);
+      $n =~ s/,/./;
+      $u = lc($u) unless length($u) == 1;
+      croak "Unit '$u' not recognized in '$m'" unless exists $units{$u};
+      $duration += $signal * $n * $units{$u};
+    }
+    else {
+      croak("Could not parse '$e'");
+    }
+  }
+
+  return sprintf('%.0f', $duration);
+}
 
 
 1;
@@ -40,7 +100,9 @@ version 0.001
 
 =head1 DESCRIPTION
 
-The module a limited set of natural language expressions into seconds.
+The module parses a limited set of natural language expressions and
+converts them into seconds.
+
 It is backwards compatible with L<Time::Duration::Parse> (passes the
 same test cases), but adds more expressions and memoization.
 
@@ -66,16 +128,16 @@ case-insensitive B<except> the single letter versions;
 
 =item *
 
+expressions in the form C<hh:mm:ss>, C<hh:mm> are also supported;
+
+=item *
+
 the tokens I<plus> or I<minus> change the signal of the expressions that
 follow them;
 
 =item *
 
-expression in the form C<hh:mm:ss>, C<hh:mm> are also supported;
-
-=item *
-
-the final value is calculating by the sum of all the expressions
+the final value is the sum of all the expressions
 taking in account the sign defined by the previous rule.
 
 =back
